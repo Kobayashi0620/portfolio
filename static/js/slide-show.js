e02e0c4299568c6073b5367b8f67c12f49c7aee2
0620wwwww <wwwww.hihihi@gmail.com>
2024-08-24 00:39:48 +0900

    update - レスポンシブ対応

--- static/js/slide-show.js
+++ static/js/slide-show.js
@@ -11,15 +11,45 @@ $('.worksSliderTop').slick({
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 769,
+            breakpoint: 2560,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 2000,
+            settings: {
+                slidesToShow: 3.5,
+            }
+        },
+        {
+            breakpoint: 1500,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1250,
             settings: {
                 slidesToShow: 2,
             }
         },
         {
-            breakpoint: 426,
+            breakpoint: 1000,
             settings: {
                 slidesToShow: 1.5,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 1.3,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1,
             }
         }
     ],
@@ -39,15 +69,45 @@ $('.worksSliderBottom').slick({
     rtl: true,
     responsive: [
         {
-            breakpoint: 769,
+            breakpoint: 2560,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 2000,
+            settings: {
+                slidesToShow: 3.5,
+            }
+        },
+        {
+            breakpoint: 1500,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1250,
             settings: {
                 slidesToShow: 2,
             }
         },
         {
-            breakpoint: 426,
+            breakpoint: 1000,
             settings: {
                 slidesToShow: 1.5,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 0,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 0,
             }
         }
     ],
